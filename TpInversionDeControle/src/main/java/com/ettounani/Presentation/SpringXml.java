30d643ff662d29eea2e39bae3f5c294c732ae197
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-02-12 12:16:00 +0100

    IOC spring version XML

--- TpInversionDeControle/src/main/java/com/ettounani/Presentation/SpringXml.java
+++ TpInversionDeControle/src/main/java/com/ettounani/Presentation/SpringXml.java
@@ -1,5 +1,13 @@
 package com.ettounani.Presentation;
 
+import com.ettounani.Metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringXml {
-    
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        IMetier metier=(IMetier) context.getBean("metier");
+        System.out.println(metier.calcul());
+    }
 }
