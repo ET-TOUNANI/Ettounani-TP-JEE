fe2f5148ab7ec9575a9fd8bca61a4bf42792b8f0
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-03-10 09:42:29 +0100

    initialisation spring boot

---
+++ Tp3 JPA Hibernate Spring Data Medecin/src/test/java/com/ettounani/Tp3JpaHibernateSpringDataMedecinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ettounani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3JpaHibernateSpringDataMedecinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
