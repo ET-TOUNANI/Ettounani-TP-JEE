c779973575e245ffd355f405519912128379241d
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-04-11 12:27:52 +0000

    remove some files

--- e-banque-service/src/main/java/ma/enset/spring_micro_service/enums/TypeCompte.java
+++
@@ -1,5 +0,0 @@
-package ma.enset.spring_micro_service.enums;
-
-public enum TypeCompte {
-    CURRENT_ACOUNT,SAVING_ACOUNT,
-}
--- e-banque-service/src/main/java/ma/enset/spring_micro_service/repository/CompteRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.enset.spring_micro_service.repository;
-
-import ma.enset.spring_micro_service.entities.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte,String> {
-}
