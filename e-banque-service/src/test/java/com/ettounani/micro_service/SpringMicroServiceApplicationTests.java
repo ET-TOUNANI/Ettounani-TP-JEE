3991ff5839f0a46eaabbfd52c0f49c2a8c52347e
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-04-11 12:28:51 +0000

    Create SpringMicroServiceApplicationTests.java

---
+++ e-banque-service/src/test/java/com/ettounani/micro_service/SpringMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ettounani.micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
